b7275a462e22dcf04a0f606ec8c782173082d0f0
TheRatcheteer <TheRatcheteer@a4b921e0-d1fd-11dd-a9c2-f1de684657c9>
2009-02-08 05:05:07 +0000

    GUI

--- EternalEngine/GUI.cs
+++ EternalEngine/GUI.cs
@@ -8,18 +8,94 @@ namespace EternalEngine
 {
     public class GUI
     {
-        public GUI()
+        public GUI(Size clientsize)
         {
-            Stamina = 100;
+            Stamina = 1;
+            ClientSize = clientsize;
+            TopLevelMenuItem = new MenuItem("Paused");
+            TopLevelMenuItem.Children.Add(new MenuItem(TopLevelMenuItem, "asdf"));
+            TopLevelMenuItem.Children.Add(new MenuItem(TopLevelMenuItem, "asdf2"));
+            TopLevelMenuItem.Children.Add(new MenuItem(TopLevelMenuItem, "asdf3"));
         }
 
-        public void Refresh(Graphics g)
+        public Size ClientSize { get; set; }
+
+        public void Draw(Graphics g)
         {
-            //Stamina bar
-            g.DrawLine(new Pen(Brushes.Red, 5f), 5, 5, Stamina + 5, 5);
+            if (IsPaused)
+            {
+                Font ftitle = new Font("Lucida Console", 36, FontStyle.Regular);
+                Font fsmall = new Font("Lucida Console", 20, FontStyle.Regular);
+                g.DrawString(CurrentMenuItem.Text, ftitle, Brushes.Black, 50, 50);
+            }
+            else
+            {
+                g.FillRegion(Brushes.LightGray, this.GetGUIBounds());
+                //Stamina bar
+                string s = "Stamina";
+                Font f = new Font(FontFamily.GenericSerif, 10, FontStyle.Bold);
+                float w = g.MeasureString(s, f).Width;
+                g.DrawLine(new Pen(Brushes.Red, 5f), w, 7, Stamina * (ClientSize.Width - w), 7);
+                g.DrawString(s, f, Brushes.Red, 0f, 0f);
+            }
         }
+
+        public void Click(Point click)
+        {
+
+        }
+
+        public void MouseMove(Point loc)
+        {
+        }
+
+        public Region GetGUIBounds()
+        {
+            if (IsPaused)
+            {
+                return new Region();
+            }
+            else
+            {
+                Region ret = new Region(new Rectangle(0, 0, ClientSize.Width, 15));
+                ret.Union(new Rectangle(0, ClientSize.Height - 15, ClientSize.Width, 15));
+                return ret;
+            }
+        }
+
+        public MenuItem TopLevelMenuItem { get; set; }
+        public MenuItem CurrentMenuItem { get; set; }
 
         public float Stamina { get; set; }
-        public bool Paused { get; set; }
+        private bool m_ispaused;
+        public bool IsPaused { get { return m_ispaused; } }
+
+        public void Pause()
+        {
+            m_ispaused = true;
+            CurrentMenuItem = TopLevelMenuItem;
+        }
+        public event EventHandler UnPause;
+    }
+
+    public class MenuItem
+    {
+        public MenuItem(MenuItem parentmenu, string text)
+        {
+            Parent = parentmenu;
+            Children = new List<MenuItem>();
+            parentmenu.Children.Add(this);
+            Text = text;
+        }
+        public MenuItem(string text)
+        {
+            Children = new List<MenuItem>();
+            Text = text;
+        }
+
+        public MenuItem Parent { get; set; }
+        public List<MenuItem> Children { get; set; }
+
+        public string Text { get; set; }
     }
 }
